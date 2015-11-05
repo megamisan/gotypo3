857d543cf9dd78bc86c7d535022f9d61b4c902fc
Pierrick Caillon <pcaillon@plan-net.fr>
2015-11-05 15:19:26 +0100

    Reconfigure available TYPO3 versions and remove install tool initialisation.

--- src/modules/300_typo3/300_typo3.sh
+++ src/modules/300_typo3/300_typo3.sh
@@ -125,7 +125,8 @@ whiptail --title "GoTYPO3 : TYPO3"                        \
          --radiolist "Select TYPO3 version to install :" \
          --noitem                                        \
          10 40 2                                         \
-         "4.5.9" 0                                       \
+         "6.2" 0                                         \
+         "7.5" 0                                         \
          2>$tempfile
 typo3_version=$(<$tempfile)
 rm $tempfile
@@ -199,135 +200,21 @@ do
     typo3_adminpwd=`</dev/urandom tr -dc a-zA-Z0-9 | head -c 8`
     typo3_installtoolpwd=`</dev/urandom tr -dc a-zA-Z0-9 | head -c 8`
     typo3_installtoolpwd_md5=`echo -n $typo3_installtoolpwd | openssl dgst -md5`
-    sql_query="CREATE DATABASE \`$typo3_db\`;
+    sql_query="CREATE DATABASE \`$typo3_db\` COLLATE utf8_unicode_ci;
                CREATE USER '$typo3_dbusr'@'localhost' IDENTIFIED BY '$typo3_dbpwd';
-               GRANT ALL PRIVILEGES ON \`$typo3_db\`.\0052 TO '$typo3_dbusr'@'localhost';
-               ALTER DATABASE \`$typo3_db\` DEFAULT CHARACTER SET utf8;
-               ALTER DATABASE \`$typo3_db\` DEFAULT COLLATE utf8_general_ci;"
+               GRANT ALL PRIVILEGES ON \`$typo3_db\`.\0052 TO '$typo3_dbusr'@'localhost';"
 
     # create the database
     echo -e $sql_query | mysql --defaults-file=/etc/mysql/debian.cnf
     
-    # download and unpack the dummy package from sourceforge
     cd $vhost_dir/httpdocs
-    wget -O "dummy-$typo3_version.tar.gz" \
-         "http://prdownloads.sourceforge.net/typo3/dummy-$typo3_version.tar.gz?download"
-    tar -xzf dummy-$typo3_version.tar.gz
-    mv dummy-$typo3_version/* ./
-    rmdir dummy-$typo3_version
-    rm dummy-$typo3_version.tar.gz
-    rm $vhost_dir/httpdocs/typo3_src
     ln -s /var/local/typo3/$typo3_symlink $vhost_dir/httpdocs/typo3_src
-    
-    # download the localconf.php and .htaccess files from GoTYPO3 server
-    rm $vhost_dir/httpdocs/typo3conf/localconf.php
-    download_file "$GOTYPO3_IFAUTH"                                         \
-                  "$GOTYPO3_SRV/modules/dummy-$typo3_version/localconf" \
-                  "$vhost_dir/httpdocs/typo3conf/localconf.php"            \
-                  "$GOTYPO3_AUTHUSR"                                        \
-                  "$GOTYPO3_AUTHPWD"
-                  
-    download_file "$GOTYPO3_IFAUTH"                                    \
-                  "$GOTYPO3_SRV/modules/dummy-$typo3_version/htaccess" \
-                  "$vhost_dir/httpdocs/.htaccess"                     \
-                  "$GOTYPO3_AUTHUSR"                                   \
-                  "$GOTYPO3_AUTHPWD"
-
-    # configure localconf.php
-    sed -i     -e "s/\${sitename}/$i/g"             \
-            -e "s/\${typo3_db}/$typo3_db/g"         \
-            -e "s/\${typo3_dbusr}/$typo3_dbusr/g"   \
-            -e "s/\${typo3_dbpwd}/$typo3_dbpwd/g"   \
-            -e "s/\${typo3_dbhost}/$typo3_dbhost/g" \
-            -e "s/\${typo3_key}/$typo3_key/g"       \
-            $vhost_dir/httpdocs/typo3conf/localconf.php
-	cp $vhost_dir/httpdocs/typo3conf/localconf.php $vhost_dir/httpdocs/typo3conf/localconf_install.php
 
     # update permissions
     chown -R $vhost_user:www-data *
     find $vhost_dir/httpdocs -type d -exec chmod 6775 \{\} \;
     find $vhost_dir/httpdocs -type f -exec chmod 0664 \{\} \;
-    
-    # configure a temporary host record and enable TYPO3 install tool
-    echo "127.0.0.100 $i" >> /etc/hosts
-    touch $vhost_dir/httpdocs/typo3conf/ENABLE_INSTALL_TOOL
-    
-    #
-    # we use wget to simulate user interactions in the install tool
-    #
 
-    # use wget to compare database
-    wget "http://$i/typo3/install/index.php?password=joh316&redirect_url=index.php%3FTYPO3_INSTALL[type]=about" \
-         --output-document=/dev/null                                                                            \
-         --cookies=on                                                                                           \
-         --save-cookies /tmp/wgetcookies                                                                        \
-         --keep-session-cookies                                                                                 \
-         --referer="http://$i/"
-
-    wget "http://$i/typo3/install/index.php?TYPO3_INSTALL[type]=database&TYPO3_INSTALL[database_type]=cmpFile|CURRENT_TABLES" \
-         --output-document=/tmp/wgetget                                                                                       \
-         --cookies=on                                                                                                         \
-         --save-cookies /tmp/wgetcookies                                                                                      \
-         --load-cookies /tmp/wgetcookies                                                                                      \
-         --keep-session-cookies                                                                                               \
-         --referer="http://$i/typo3/install/index.php?TYPO3_INSTALL[type]=database"
-
-    # generate post data needed to select database changes
-    echo -n "TYPO3_INSTALL[database_type]=cmpFile|CURRENT_TABLES" > /tmp/wgetpost
-
-    for j in `grep "<input type=\"checkbox\".*name=\"" /tmp/wgetget`
-    do
-        case $j in
-        name=*)
-            echo -n "&" >> /tmp/wgetpost
-            echo $j | cut -d \" -f 2 | tr -d "\n" >> /tmp/wgetpost
-            echo -n "=" >> /tmp/wgetpost
-            ;;
-        value=*)
-            echo $j | cut -d \" -f 2 | tr -d "\n" >> /tmp/wgetpost
-            ;;
-        esac
-    done
-
-    rm /tmp/wgetget
-    wget_post=$(</tmp/wgetpost)
-    rm /tmp/wgetpost
-
-    # apply changes to the database
-    wget "http://$i/typo3/install/index.php?TYPO3_INSTALL[type]=database"                                                               \
-         --output-document=/dev/null                                                                                                    \
-         --cookies=on                                                                                                                   \
-         --load-cookies /tmp/wgetcookies                                                                                                \
-         --keep-session-cookies                                                                                                         \
-         --referer="http://$i/typo3/install/index.php?TYPO3_INSTALL[type]=database&TYPO3_INSTALL[database_type]=cmpFile|CURRENT_TABLES" \
-         --post-data $wget_post
-
-    # generate post data to add admin user
-    wget_post="TYPO3_INSTALL%5Bdatabase_type%5D=adminUser%7C"\
-"&TYPO3_INSTALL%5Bdatabase_adminUser%5D%5Busername%5D=$vhost_user"\
-"&TYPO3_INSTALL%5Bdatabase_adminUser%5D%5Bpassword%5D=$typo3_adminpwd"\
-"&TYPO3_INSTALL%5Bdatabase_adminUser%5D%5Bpassword2%5D=$typo3_adminpwd"
-
-    # add admin user
-    wget "http://$i/typo3/install/index.php?TYPO3_INSTALL[type]=database"                                                   \
-         --output-document=/dev/null                                                                                        \
-         --cookies=on                                                                                                       \
-         --load-cookies /tmp/wgetcookies                                                                                    \
-         --keep-session-cookies                                                                                             \
-         --referer="http://$i/typo3/install/index.php?TYPO3_INSTALL[type]=database&TYPO3_INSTALL[database_type]=adminUser|" \
-         --post-data $wget_post
-
-    rm /tmp/wgetcookies
-
-    # update install tool password
-    sed -i -e "s/bacb98acf97e0b6112b1d1b650b84971/$typo3_installtoolpwd_md5/g" \
-        $vhost_dir/httpdocs/typo3conf/localconf.php
-	cp $vhost_dir/httpdocs/typo3conf/localconf.php $vhost_dir/httpdocs/typo3conf/localconf_install.php
- 
-    # delete temporary host record and disable install tool
-    rm $vhost_dir/httpdocs/typo3conf/ENABLE_INSTALL_TOOL
-    sed -i -e "/127.0.0.100 $i/d" /etc/hosts
-    
     # update report
     echo "Database : $typo3_db" >> /opt/ics/gotypo/report_$i
     echo "Database user : $typo3_dbusr" >> /opt/ics/gotypo/report_$i
@@ -335,7 +222,6 @@ do
     echo "TYPO3 admin : $vhost_user" >> /opt/ics/gotypo/report_$i
     echo "TYPO3 admin password : $typo3_adminpwd" >> /opt/ics/gotypo/report_$i
     echo "TYPO3 install tool password : $typo3_installtoolpwd" >> /opt/ics/gotypo/report_$i
-    echo "TYPO3 key : $typo3_key" >> /opt/ics/gotypo/report_$i
 done
 
 trap - INT TERM EXIT
